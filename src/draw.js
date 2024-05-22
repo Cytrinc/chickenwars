40687c8406b2554f6331fcd9f395a54c719a970a
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-05-21 17:50:09 -0700

    damnit

--- src/draw.js
+++ src/draw.js
@@ -1,4 +1,4 @@
-var el = document.getElementById("myCanvas");
+var el = document.getElementById("stage");
 var ctx = el.getContext("2d");
 ctx.font = "30px Arial";
 ctx.strokeText("Welcome to Chicken Wars", 10, 50);
